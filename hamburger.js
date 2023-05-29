18f9464fb0cc352488ff573cacd39e50f5729da8
Tife <boluakindele12@gmail.com>
2023-05-29 09:55:14 +0100

    few edits

--- hamburger.js
+++ hamburger.js
@@ -2,6 +2,5 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
-  // hamburger.classList.toggle('active');
   menu.classList.toggle('active')
 })
